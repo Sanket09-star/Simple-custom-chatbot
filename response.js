f1e096807f33c9741b1406b597b5c2f55ed86532
$anket <88323848+Sanket09-star@users.noreply.github.com>
2023-06-26 14:39:13 +0530

    Add files via upload

---
+++ response.js
@@ -0,0 +1,8 @@
+const responseObj = {
+  hi: "How may i help you!!",
+  hello: "Hey ! How are you doing ?",
+  hey: "Hey! What's Up",
+  1: "jai shri ram",
+  today: new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+};
